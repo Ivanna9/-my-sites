d96c87bdaff6e6843b53746da9aec622a4f98676
Ivanna _ <ivantina@gmail.com>
2022-12-31 22:36:41 +0100

    COMMIT

--- First sites/PHP/index.php
+++ First sites/PHP/index.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Some game</title>
     <link rel="stylesheet" href="/First sites/CSS/end.css">
-    <script scr="/First sites/JS/main.js"></script>
+   
 </head>
 <body>
     <div class= "content">
         
-       <div class="area">
-
+       <div id="area">
+       <script scr="/First sites/JS/main.js"></script>
        </div> 
     </div>
 </body>
