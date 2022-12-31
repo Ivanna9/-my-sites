2908a4fede7dbcab998c03273c9bc35e2873e9b8
Ivanna _ <ivantina@gmail.com>
2022-12-31 22:18:07 +0100

    commit

---
+++ First sites/PHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Some game</title>
+    <link rel="stylesheet" href="/First sites/CSS/end.css">
+    <script scr="/First sites/JS/main.js"></script>
+</head>
+<body>
+    <div class= "content">
+        
+       <div class="area">
+
+       </div> 
+    </div>
+</body>
+</html>
